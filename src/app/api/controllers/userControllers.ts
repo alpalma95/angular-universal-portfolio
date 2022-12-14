5e0055136bac19085ca38b4eed2f76e28c93d58e
Álvaro Palma Mancheño <alvaro.palma@bosonit.com>
2022-12-14 10:06:39 +0100

    add try catch blocks

--- src/app/api/controllers/userControllers.ts
+++ src/app/api/controllers/userControllers.ts
@@ -11,39 +11,47 @@ export const getUsers = async (req: any, res: any) => {
 };
 
 export const createUser = async (req: any, res: any) => {
-  const body = req.body;
-  const newUser = new User(
-    body.email,
-    body.password,
-    body.passwordConfirm,
-    body.name,
-    body.secret
-  );
-  if (!newUser.checkPasswordMatch()) return;
+  try {
+    const body = req.body;
+    const newUser = new User(
+      body.email,
+      body.password,
+      body.passwordConfirm,
+      body.name,
+      body.secret
+    );
+    if (!newUser.checkPasswordMatch()) return;
 
-  newUser.encryptPassword();
-  const user = await prisma.user.create({
-    data: newUser,
-  });
+    newUser.encryptPassword();
+    const user = await prisma.user.create({
+      data: newUser,
+    });
 
-  res.status(200).json(user);
+    res.status(200).json(user);
+  } catch (err: any) {
+    res.status(err.status).json({ message: 'Something went wrong!' });
+  }
 };
 
 export const loginUser = async (req: any, res: any) => {
-  const user = await prisma.user.findUnique({
-    where: {
-      email: req.body.email,
-    },
-  });
-  if (!user || !bcrypt.compareSync(req.body.password, user?.password))
-    res.status(404).json({
-      satus: 'error',
-      message: 'Invalid credentials',
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        email: req.body.email,
+      },
     });
+    if (!user || !bcrypt.compareSync(req.body.password, user?.password))
+      res.status(404).json({
+        satus: 'error',
+        message: 'Invalid credentials',
+      });
 
-  if (bcrypt.compareSync(req.body.password, user?.password)) {
-    res
-      .status(200)
-      .json({ name: user?.name, email: user?.email, secret: user?.secret });
+    if (bcrypt.compareSync(req.body.password, user?.password)) {
+      res
+        .status(200)
+        .json({ name: user?.name, email: user?.email, secret: user?.secret });
+    }
+  } catch (err: any) {
+    res.status(err.status).json({ message: 'Something went wrong!' });
   }
 };
