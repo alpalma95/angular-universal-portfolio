b1746ed775ebbe9f19a41bdbfa12e02a39e935ad
Álvaro Palma Mancheño <alvaro.palma@bosonit.com>
2022-12-14 10:10:35 +0100

    delete unused route

--- src/app/api/routes/userRouter.ts
+++ src/app/api/routes/userRouter.ts
@@ -1,12 +1,7 @@
 import * as express from 'express';
-import {
-  getUsers,
-  createUser,
-  loginUser,
-} from '../controllers/userControllers';
+import { createUser, loginUser } from '../controllers/userControllers';
 
 export const userRouter = express.Router();
 
-userRouter.route('/').get(getUsers);
 userRouter.route('/signup').post(createUser);
 userRouter.route('/login').post(loginUser);
